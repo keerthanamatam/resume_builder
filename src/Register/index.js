54f68741e8a979a4ae6e70089c603c0da2e08ad9
keerthanamatam <keerthanamatam3107@gmail.com>
2025-09-07 11:11:33 +0530

    Added validations to register page

--- src/Register/index.js
+++ src/Register/index.js
@@ -6,6 +6,80 @@ import { Person, Email, LocalPhone, Lock, VisibilityOff, Visibility } from '@mui
 const Register = () => {
     const [showPassword, setShowPassword] = useState(false)
     const [showConfirm, setShowConfirm] = useState(false)
+    const [error, setError] = useState({})
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        phoneNumber: '',
+        password: '',
+        confirmPassword: ''
+    })
+
+    const handleSubmit = () => {
+        if (formData.name === '') {
+            setError((prev) => ({
+                ...prev,
+                ['name']: 'Name is required'
+            }))
+        }
+        console.log(formData)
+
+
+    }
+    const handleChange = (e) => {
+        const { name, value } = e.target
+        setFormData((prev) => ({
+            ...prev,
+            [name]: value
+        }))
+        const errorMsg = validate(name, value)
+        setError((prev) => ({
+            ...prev,
+            [name]: errorMsg
+        }))
+    }
+
+    const validate = (name, value) => {
+        switch (name) {
+            case 'name':
+                if (!value)
+                    return 'Name is required'
+                return ''
+            case 'email':
+                const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+                if (!value) {
+                    return "Email is required";
+                } else if (!emailRegex.test(value)) {
+                    return "Enter a valid email address";
+                }
+                return "";
+            case 'phoneNumber':
+                const phoneRegex = /^[0-9]{10}$/;
+                if (!value) {
+                    return "Phone number is required"
+                }
+                else if (!phoneRegex.test(value)) {
+                    return "Enter a valid phone Number"
+                }
+                return ''
+            case 'password':
+                if (!value) {
+                    return 'Password is required'
+                }
+                return ''
+            case 'confirmPassword':
+                if (!value) {
+                    return 'confirm password is required'
+                }
+                else {
+                    if (value !== formData.password) {
+                        return "Passwords do not match"
+                    }
+                }
+                return ''
+        }
+
+    }
 
     return (
         <Grid container justifyContent='center' alignItems='center' style={{
@@ -29,69 +103,117 @@ const Register = () => {
                     Create Account
                 </Typography>
 
-                <form>
+                <form onSubmit={handleSubmit}>
                     <Grid container spacing={2} direction={"column"}>
                         <Grid>
-                            <TextField fullWidth variant="outlined" label='Enter Name' sx={{ backgroundColor: '#f2f2f2' }} InputProps={{
-                                startAdornment: (
-                                    <InputAdornment position="start" >
-                                        <Person color="primary" />
-                                    </InputAdornment>
-                                )
-                            }} />
+                            <TextField fullWidth
+                                name="name"
+                                error={error.name}
+                                helperText={error.name}
+                                required variant="outlined"
+                                onChange={handleChange}
+                                label='Enter Name' sx={{ backgroundColor: '#f2f2f2' }} InputProps={{
+                                    startAdornment: (
+                                        <InputAdornment position="start" >
+                                            <Person color="primary" />
+                                        </InputAdornment>
+                                    )
+                                }} />
                         </Grid>
 
                         <Grid>
-                            <TextField fullWidth variant="outlined" label='Enter Email' sx={{ backgroundColor: '#f2f2f2' }} InputProps={{
-                                startAdornment: (<InputAdornment position="start">
-                                    <Email color="primary" />
-                                </InputAdornment>)
-                            }} />
+                            <TextField fullWidth
+                                variant="outlined"
+                                required name="email"
+                                error={error.email}
+                                helperText={error.email}
+                                onChange={handleChange}
+                                label='Enter Email'
+                                sx={{ backgroundColor: '#f2f2f2' }} InputProps={{
+                                    startAdornment: (<InputAdornment position="start">
+                                        <Email color="primary" />
+                                    </InputAdornment>)
+                                }} />
                         </Grid>
 
                         <Grid>
-                            <TextField fullWidth variant="outlined" label='Enter Phone Number' sx={{ backgroundColor: '#f2f2f2' }} InputProps={{
-                                startAdornment: (
-                                    <InputAdornment position="start">
-                                        <LocalPhone color="primary" />
-                                    </InputAdornment>
-                                )
-                            }} />
+                            <TextField fullWidth
+                                variant="outlined"
+                                required
+                                error={error.phoneNumber}
+                                helperText={error.phoneNumber}
+                                name="phoneNumber"
+                                onChange={handleChange}
+                                label='Enter Phone Number'
+                                sx={{ backgroundColor: '#f2f2f2' }} InputProps={{
+                                    startAdornment: (
+                                        <InputAdornment position="start">
+                                            <LocalPhone color="primary" />
+                                        </InputAdornment>
+                                    )
+                                }} />
                         </Grid>
                         <Grid>
-                            <TextField fullWidth variant="outlined" type={showPassword ? 'text' : 'password'} label='Enter Password' sx={{ backgroundColor: '#f2f2f2' }} InputProps={{
-                                startAdornment: (
-                                    <InputAdornment position="start">
-                                        <Lock color="primary" />
-                                    </InputAdornment>
-                                ),
-                                endAdornment: (
-                                    <InputAdornment position="end">
-                                        <IconButton onClick={() => setShowPassword(!showPassword)}>
-                                            {showPassword ? <Visibility /> : <VisibilityOff />}
-                                        </IconButton>
-                                    </InputAdornment>
-                                )
-                            }} />
+                            <TextField fullWidth
+                                variant="outlined"
+                                required name="password"
+                                error={error.password}
+                                helperText={error.password}
+                                onChange={handleChange}
+                                type={showPassword ? 'text' : 'password'}
+                                label='Enter Password'
+                                sx={{ backgroundColor: '#f2f2f2' }} InputProps={{
+                                    startAdornment: (
+                                        <InputAdornment position="start">
+                                            <Lock color="primary" />
+                                        </InputAdornment>
+                                    ),
+                                    endAdornment: (
+                                        <InputAdornment position="end">
+                                            <IconButton onClick={() => setShowPassword(!showPassword)}>
+                                                {showPassword ? <Visibility /> : <VisibilityOff />}
+                                            </IconButton>
+                                        </InputAdornment>
+                                    )
+                                }} />
                         </Grid>
                         <Grid>
-                            <TextField fullWidth variant="outlined" type={showConfirm ? 'text' : 'password'} label='Enter confirm Password' sx={{ backgroundColor: '#f2f2f2' }} InputProps={{
-                                startAdornment: (
-                                    <InputAdornment position="start">
-                                        <Lock color="primary" />
-                                    </InputAdornment>
-                                ),
-                                endAdornment: (
-                                    <InputAdornment position="end">
-                                        <IconButton onClick={() => setShowConfirm(!showConfirm)}></IconButton>
-                                        {showConfirm ? <Visibility /> : <VisibilityOff />}
-                                    </InputAdornment>
-                                )
-                            }} />
+                            <TextField fullWidth
+                                onChange={handleChange}
+                                required name="confirmPassword"
+                                variant="outlined"
+                                error={error.confirmPassword}
+                                helperText={error.confirmPassword}
+                                type={showConfirm ? 'text' : 'password'}
+                                label='Enter confirm Password'
+                                sx={{ backgroundColor: '#f2f2f2' }} InputProps={{
+                                    startAdornment: (
+                                        <InputAdornment position="start">
+                                            <Lock color="primary" />
+                                        </InputAdornment>
+                                    ),
+                                    endAdornment: (
+                                        <InputAdornment position="end">
+                                            <IconButton onClick={() => setShowConfirm(!showConfirm)}></IconButton>
+                                            {showConfirm ? <Visibility /> : <VisibilityOff />}
+                                        </InputAdornment>
+                                    )
+                                }} />
                         </Grid>
 
                         <Grid>
-                            <Button variant="contained" color="primary" sx={{ paddingLeft: '100px', paddingRight: '120px', fontWeight: 'bold', borderRadius: '8px', paddingTop: '10px', paddingBottom: '10px' }}>Register</Button>
+                            <Button
+                                variant="contained"
+                                color="primary"
+                                onClick={handleSubmit}
+                                sx={{
+                                    paddingLeft: '100px',
+                                    paddingRight: '120px',
+                                    fontWeight: 'bold',
+                                    borderRadius: '8px',
+                                    paddingTop: '10px',
+                                    paddingBottom: '10px'
+                                }}>Register</Button>
                         </Grid>
 
 
