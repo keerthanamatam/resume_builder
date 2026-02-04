208755e6690323a81256931d76d4de3a56878dc6
keerthanamatam <keerthanamatam3107@gmail.com>
2026-02-04 07:50:13 +0530

    Css changes in dashboard

--- src/Dashboard/index.js
+++ src/Dashboard/index.js
@@ -1,18 +1,20 @@
-import { Card, CardActions, CardContent, Typography, Button } from '@mui/material'
-import './index.css'
-import  NavBar  from '../Navbar/index'
+import { Typography } from '@mui/material';
+import './index.css';
+import NavBar from '../Navbar';
+import { grey } from '@mui/material/colors';
 
 const Dashboard = () => {
-    return (
-        <>
-        <div className='dashboard-layout'>
-            <NavBar />
-            <div className='container'>
-                <>Hai</>
-            </div>
-        </div>
-        </>
-    )
-}
+  return (
+    <div className="dashboard-layout">
+      <NavBar />
 
-export default Dashboard
+      <div className="container">
+        <Typography sx={{ fontWeight: 600, fontSize: 20, color: "grey", fontFamily: "satisfies" }}>
+          Welcome Back, Jhon!
+        </Typography>
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
